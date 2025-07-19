62154e0818c9e0d60170ca197a991554b0ed6f86
Yamil Castillo <yamilcs@gmail.com>
2025-07-19 01:39:10 -0400

    Verificando Datos login

--- config/routes/loginRoutes.js
+++ config/routes/loginRoutes.js
@@ -2,12 +2,11 @@ import express from "express";
 
 import { getActivity } from "../../middlewares/reports.js";
 import { loginUser } from "../../src/api/v1/controllers/loginController.js";
-import { validateParametersLogin } from "../../middlewares/validateParameterLogin.js";
-import { isLogin } from "../../middlewares/isLogin.js";
+
 
 
 const router = express.Router();
 
-router.post('/login',  validateParametersLogin, getActivity, loginUser )
+router.post('/login', getActivity, loginUser )
 
 export default router;
